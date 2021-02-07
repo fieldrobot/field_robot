b9bfcba75eba364041543ee93584ee87177e259d
Simon Sure <info@simonsure.com>
2021-02-07 19:53:54 +0100

    gazebo integration in field-robot packgae

--- dev_ws/src/field-robot/setup.py
+++ dev_ws/src/field-robot/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'field-robot'
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, glob('launch/*.launch.xml')),
+        ('share/' + package_name + '/worlds', glob('worlds/*.model')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
