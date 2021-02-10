3a18797906ab91dcd6f18f3ac3bdb3a363b6833d
Simon Sure <info@simonsure.com>
2021-02-10 19:53:34 +0100

    creating the simulation launch file as a python script

---
+++ dev_ws/src/field-robot/launch/simulation.launch.py
@@ -0,0 +1,27 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    world = os.path.join(get_package_share_directory('field-robot'), 'worlds', 'field_with_robot.world')
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py')
+            ),
+            launch_arguments={'world': world}.items(),
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('field-robot'), 'robot_state_publisher.launch.py')
+            ),
+        ),
+    ])
