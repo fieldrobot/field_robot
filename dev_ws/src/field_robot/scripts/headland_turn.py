0ca85b79bc3f42a439cb1dbea5cf16e5269e5fd7
Simon Sure <info@simonsure.com>
2021-04-03 18:18:58 +0200

    optimizing row follower and preparing headland turn

--- dev_ws/src/field_robot/scripts/headland_turn.py
+++ dev_ws/src/field_robot/scripts/headland_turn.py
@@ -30,7 +30,7 @@ class EmptySpaceFollowerServer(Node):
         
         self.bridge = cv_bridge.CvBridge()
 
-        self.working = False
+        self.working = True
 
         self.publish = self.create_publisher(Twist, '/cmd_vel', 10)
 
@@ -112,28 +112,7 @@ class EmptySpaceFollowerServer(Node):
         
         #### regulating the velocity command
         #calculation the error
-        error = (w/2)-sm #negative: to far to the left -> right &&&&&&& positive: to far to the right -> left
-
-        #preparing the message to be send
-        twist_msg = Twist()
-        twist_msg.linear.x = 0.5
-        twist_msg.linear.y = 0.0
-        twist_msg.linear.z = 0.0
-        twist_msg.angular.x = 0.0
-        twist_msg.angular.y = 0.0
-        twist_msg.angular.z = 0.0
-
-        #targets/configuaration
-        linear_target = 0.2
-        angular_d = 0.005
-        linear_d = 1
-
-        #p-control
-        twist_msg.angular.z = error * angular_d
-        #twist_msg.linear.x = linear_target + linear_d / error
-
-        #publishing the velocity commands
-        self.publish.publish(twist_msg)
+        error = (w/2)-sm #positive: to far to the left -> right &&&&&&& negative: to far to the right -> left
 
         cv2.waitKey(1)
 
