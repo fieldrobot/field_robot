e3ea197c87f63e7917e40b7634e1885d58664fe0
Simon Sure <info@simonsure.com>
2021-04-05 13:37:38 +0200

    implementing the action server for the empty space follower, setting the action file how it is supposed to work and beginning to insert comments

--- dev_ws/src/field_robot/scripts/empty_space_follower_two.py
+++ dev_ws/src/field_robot/scripts/empty_space_follower_two.py
@@ -1,73 +1,112 @@
-import os
-import sys
-
-import rclpy
-from rclpy.action import ActionServer
-from rclpy.node import Node
-from rclpy.qos import QoSDurabilityPolicy, QoSHistoryPolicy, QoSReliabilityPolicy, QoSProfile
-import rclpy.time
-from rclpy.duration import Duration
-
-from ament_index_python.packages import get_package_share_directory
-
-from sensor_msgs.msg import Image
-from geometry_msgs.msg import Twist
-import std_msgs.msg
-from field_robot.action import EmptySpaceFollower
-
+# importing libraries used for computation
 import cv2
 import cv_bridge
 import numpy
-import math
-from time import sleep
-from timeit import default_timer as timer
 
+# necessary to make parallel processes sleeping so that they do not overwork
+import time
+
+# importing the client library for python: default for every node
+import rclpy
+# importing the node, necessary to register this script as a node
+from rclpy.node import Node
+# importing libraries to keep the aciton server running
+from rclpy.action import ActionServer, CancelResponse # default action server and making canceling possible
+# necessary to execute callbacks at the same time without one blocking the other ones
+from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.executors import MultiThreadedExecutor
+
+# quality of service profily needs to be created as the image is published not with default configuration
+from rclpy.qos import QoSDurabilityPolicy, QoSHistoryPolicy, QoSReliabilityPolicy, QoSProfile
+
+# importing message and action files to receive and send data via ROS2
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
+from field_robot.action import EmptySpaceFollower
+
+# class handling everything concerning empty space following
 class EmptySpaceFollowerServer(Node):
+
+    # constructor for the empty space follower
     def __init__(self):
-        #start node
+        #starting node and naming it
         super().__init__('empty_space_follower')
+
         self.get_logger().info('starting...')
         
+        # creating the cv birdge for image processing
         self.bridge = cv_bridge.CvBridge()
 
-        self.working = True
+        # this variable is refered to by the image callback and action server and represents the state of the system
+        # true: navigaiton is working and cmd_vel is published
+        # false: navigation is halted and no velocity commands are published, some other node is responsible for publishing those
+        self.working = False
 
+        # setting up the publisher for velocity commands
         self.publish = self.create_publisher(Twist, '/cmd_vel', 10)
 
+        # registering the action server, defining its callbacks and declaring it's callback group (necessary for parallel execution)
         self.action_server = ActionServer(
-            self,
-            EmptySpaceFollower,
-            'empty_space_follower',
-            self.action_callback
+            node=self, # the node (this class) to associate with
+            action_type=EmptySpaceFollower, # action message type
+            action_name='empty_space_follower', # action name
+            execute_callback=self.action_callback, # the callback called when action server request accepted (by default: all are accepted)
+            callback_group=ReentrantCallbackGroup(), # making parallel execution possible by multithreading
+            cancel_callback=self.cancel_callback # the cancel callback makes is possible to stop the navigation
         )
 
+        # declaring the qos_profile for image subscription
         self._qos_profile = QoSProfile(
-            history=QoSHistoryPolicy.KEEP_ALL
+            history=QoSHistoryPolicy.SYSTEM_DEFAULT
         )
         self._qos_profile.history = QoSHistoryPolicy.SYSTEM_DEFAULT
         self._qos_profile.depth = 10
         self._qos_profile.durability = QoSDurabilityPolicy.SYSTEM_DEFAULT
         self._qos_profile.reliability = QoSReliabilityPolicy.SYSTEM_DEFAULT
+        # subscribing the image and declaring callbacks and the qos profile
         self.subscription = self.create_subscription(
-            msg_type=Image,
-            topic='field_robot/gazebo_cam/image_raw',
-            callback=self.image_callback,
-            qos_profile=self._qos_profile
+            msg_type=Image, # the message file the received data has
+            topic='field_robot/gazebo_cam/image_raw', # the topic to be subscribed
+            callback=self.image_callback, # declaring the callback for image processing 
+            callback_group=ReentrantCallbackGroup(), # making parallel execution possible my multithreading
+            qos_profile=self._qos_profile # the qos profile
         )
 
         self.get_logger().info('initialized')
 
+    # called when action server request ist accepted -> implementing the actual action
     def action_callback(self, goal_handle):
         self.get_logger().info('Setting goal...')
+        # setting the working variable to true so that the image processing starts and cmd_vel is published
         self.working = True
         self.get_logger().info('Successfully initiated navigation')
-        goal_handle.succeed()
+        # returning the result of the action message file is mandatory
         result = EmptySpaceFollower.Result()
-        return result
+        # checks constantly whether there there is a request that the execution is canceled and navigation is stopped
+        while True:
+            time.sleep(0.2) # to reduce computational needs: pause
+            # is executed if navigaiton is to be stopped/cancelled
+            if goal_handle.is_cancel_requested:
+                # setting the working variable to that no cmd_vel commands are published any more
+                self.working = False
+                time.sleep(0.2)
 
+                # publishing 0 values for all aspects so that the robot stops
+                twist_msg = Twist()
+                self.publish.publish(twist_msg)
+
+                # confirming cancel
+                goal_handle.canceled()
+                return result
+        # the action can not return successful as this script is just navigation continuously
+
+    # declaring in the callback called when navigation is to be stopped that all requests to do so are accepted
+    def cancel_callback(self, cancel_requests):
+        return CancelResponse.ACCEPT
+
+    # the image processing/actual navigation
     def image_callback(self, msg):
         self.get_logger().info('Calling image callback...')
-        print(msg.header.stamp)
         if self.working == False:
             self.get_logger().info('Inactive')
             return
@@ -108,11 +147,11 @@ class EmptySpaceFollowerServer(Node):
 
         #### visualizing that middle
         image_with_point = cv2.circle(image_dilated, (int(sm), 50), 20, (255), -20)
-        cv2.imshow("kevin", image_with_point)
+        #cv2.imshow("kevin", image_with_point)
         
         #### regulating the velocity command
         #calculation the error
-        error = (w/2)-sm #negative: to far to the left -> right &&&&&&& positive: to far to the right -> left
+        self.error = (w/2)-sm #negative: to far to the left -> right &&&&&&& positive: to far to the right -> left
 
         #preparing the message to be send
         twist_msg = Twist()
@@ -124,18 +163,23 @@ class EmptySpaceFollowerServer(Node):
         twist_msg.angular.z = 0.0
 
         #targets/configuaration
-        linear_target = 0.2
+        # unused so far: linear_target = 0.2
         angular_d = 0.002
-        linear_d = 1
+        # unused so far: linear_d = 1
 
         #p-control
-        twist_msg.angular.z = error * angular_d
+        twist_msg.angular.z = self.error * angular_d
         #twist_msg.linear.x = linear_target + linear_d / error
+
+        # checking whether publishing is still acceptable
+        if self.working == False:
+            self.get_logger().info('Stopped publishing due to canceled action.')
+            return
 
         #publishing the velocity commands
         self.publish.publish(twist_msg)
 
-        cv2.waitKey(1)
+        #cv2.waitKey(1)
 
     def mindestens_einmal(self, streifen, image):
         count = 0
@@ -176,7 +220,8 @@ class EmptySpaceFollowerServer(Node):
 def main():
     rclpy.init()
     follower = EmptySpaceFollowerServer()
-    rclpy.spin(follower)
+    executor = MultiThreadedExecutor()
+    rclpy.spin(follower, executor=executor)
     rclpy.shutdown()
 
 if __name__ == '__main__':
