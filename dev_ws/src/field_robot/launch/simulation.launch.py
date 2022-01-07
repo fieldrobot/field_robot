9685a46f9ca99d61a37e5eee7224c5dc2ed7d976
Simon Sure <info@simonsure.com>
2022-01-07 10:15:58 +0100

    enabling simulation gui

--- dev_ws/src/field_robot/launch/simulation.launch.py
+++ dev_ws/src/field_robot/launch/simulation.launch.py
@@ -21,7 +21,7 @@ def generate_launch_description():
             ),
             launch_arguments={
                 'world': world,
-                'gui': 'false'
+                'gui': 'true'
             }.items(),
         ),
 
