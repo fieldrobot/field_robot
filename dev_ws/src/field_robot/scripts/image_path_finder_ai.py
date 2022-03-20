987ee22cb10f1a5d573688f878bc424881953763
Simon Sure <info@simonsure.com>
2022-03-20 19:12:37 +0100

    adding essential structure to the image_path_finder_ai

--- dev_ws/src/field_robot/scripts/image_path_finder_ai.py
+++ dev_ws/src/field_robot/scripts/image_path_finder_ai.py
@@ -1,28 +1,51 @@
 #!/usr/bin/python3
 
 import os
+from ament_index_python.packages import get_package_share_directory
+
 import rclpy
 from rclpy.node import Node
 
 from sensor_msgs.msg import Image
 
+import tensorflow as tf
+
+
+
 class ImageAIPathFinder(Node):
     def __init__(self):
-        super().__init__("image_path_finder_ai")
+        super().__init__('image_path_finder_ai')
+        self.declare_parameter('image_src')
+        self.declare_parameter('image_dst')
+        
+        # ros publisher & subscriber
         self.subscription = self.create_subscription(
             Image,
-            'topic',
+            self.get_parameter('image_src'),
             self.image_callback,
             10)
         self.publisher = self.create_publisher(
             Image,
-            'topic',
+            self.get_parameter('image_dst'),
             10)
         self.subscription
+
+        # tensorflow setup
+        self.get_logger().info("TensorFlow version: ", tf.__version__)
+        models_dir = os.path.join(get_package_share_directory('field_robot'), 'config', 'ai_models', 'image_path_finder')
+        model = tf.keras.models.load_model(models_dir)
 
     def image_callback(self, msg):
         self.publisher.publish(msg)
         self.get_logger().info("image callback called")
+
+        # convert msg to numpy array
+
+        # run model
+
+        # convert result to msg
+
+        # publish result
 
 def main(args=None):
     # Start node
