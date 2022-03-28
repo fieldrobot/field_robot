44b287ae4a5a8cb6d37acb7986f584f93b9357ef
Simon Sure <info@simonsure.com>
2022-03-28 17:46:58 +0200

    correcting spelling mistake

--- dev_ws/src/field_robot/launch/simulation.launch.py
+++ dev_ws/src/field_robot/launch/simulation.launch.py
@@ -9,7 +9,7 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch.acions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument
 
 
 def generate_launch_description():
