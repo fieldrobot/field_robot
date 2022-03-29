0f6ce5a0cff62b939d95058a10652ff26ec17aae
Simon Sure <info@simonsure.com>
2022-03-29 19:42:43 +0200

    reformat operating_services launch file

--- dev_ws/src/field_robot/launch/operating_services.launch.py
+++ dev_ws/src/field_robot/launch/operating_services.launch.py
@@ -14,41 +14,41 @@ from launch.actions import DeclareLaunchArgument
 def generate_launch_description():
 
     use_sim_time = DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='true',
-            description='Use simulation (Gazebo) clock if true'
-        )
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true'
+    )
 
     urdf = DeclareLaunchArgument(
-            'urdf',
-            default_value=os.path.join(get_package_share_directory('field_robot'), 'models', 'robot', 'robot.urdf'),
-        )
+        'urdf',
+        default_value=os.path.join(get_package_share_directory('field_robot'), 'models', 'robot', 'robot.urdf'),
+    )
 
     robot_state_publisher = Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            namespace='robot',
-            output='screen',
-            parameters=[
-                {'use_sim_time': LaunchConfiguration('use_sim_time')},
-            ],
-            arguments=[
-                LaunchConfiguration('urdf')
-            ],
-        )
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        namespace='robot',
+        output='screen',
+        parameters=[
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
+        ],
+        arguments=[
+            LaunchConfiguration('urdf')
+        ],
+    )
 
     odometry = Node(
-            package='robot_localization',
-            executable='ekf_node',
-            name='odometry_fiter',
-            #namespace='robot',
-            output='screen',
-            parameters=[
-                os.path.join(get_package_share_directory('field_robot'), 'config/odometry.yaml'),
-                {'use_sim_time': LaunchConfiguration('use_sim_time')},
-            ]
-        )
+        package='robot_localization',
+        executable='ekf_node',
+        name='odometry_fiter',
+        #namespace='robot',
+        output='screen',
+        parameters=[
+            os.path.join(get_package_share_directory('field_robot'), 'config/odometry.yaml'),
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
+        ]
+    )
 
     #urdf = os.path.join(
     #    get_package_share_directory('field_robot'), 'models', 'robot', 'robot.urdf')
