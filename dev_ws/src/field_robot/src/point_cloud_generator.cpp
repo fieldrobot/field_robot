b03e0c80d58d6389b69c1d431d93731329996282
Simon Sure <info@simonsure.com>
2022-04-11 18:55:17 +0200

    making code compilable

--- dev_ws/src/field_robot/src/point_cloud_generator.cpp
+++ dev_ws/src/field_robot/src/point_cloud_generator.cpp
@@ -1,28 +1,34 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+
 #include "rclcpp/rclcpp.hpp"
-#include "sensor_msg/msg/Image.hpp"
+#include "sensor_msgs/msg/image.hpp"
+
+using std::placeholders::_1;
 
 class PointCloudGenerator : public rclcpp::Node
 {
     public:
         PointCloudGenerator() : Node("point_cloud_generator")
         {
-            image_usbscriber_ = this->create_subscription<sensor_msg::msg::Image>("topic", 10, std::bind(&PointCloudGenerator::image_callback, this, _1));
-            image_publisher_ = this->create_publisher<sensor_msg::msg::Image>("topic", 10);
+            image_subscriber_ = this->create_subscription<sensor_msgs::msg::Image>("topic", 10, std::bind(&PointCloudGenerator::image_callback, this, _1));
+            image_publisher_ = this->create_publisher<sensor_msgs::msg::Image>("topic", 10);
         }
-        void image_callback(const sensor_msg::msg::Image::SharedPtr msg);
+        //void image_callback(const sensor_msgs::msg::Image::SharedPtr msg);
 
     private:
-        void image_callback(const sensor_msg::msg::Image::SharedPtr msg) const
+        void image_callback(const sensor_msgs::msg::Image::SharedPtr msg) const
         {
-            RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->header.c_str());
-            auto message = sensor_msg::msg::Image();
-            iamge_publisher_->publish(message);
+            RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->encoding.c_str());
+            auto message = sensor_msgs::msg::Image();
+            image_publisher_->publish(message);
         }
-        rclcpp::Subscription<sensor_msg::msg::Image>::SharedPtr image_subscriber_;
-        rclcpp::Publisher<sensor_msg::msg::Image>::SharedPtr image_publisher_;
-}
+        rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_subscriber_;
+        rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_publisher_;
+};
 
-int main(int, argc, char * argv[])
+int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<PointCloudGenerator>());
