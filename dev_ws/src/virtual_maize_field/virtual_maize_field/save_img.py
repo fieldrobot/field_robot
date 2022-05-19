7af11e7c77c458425ecb1b39d05e077c605fb716
isiko <koerner@informatik-ag.net>
2022-05-19 22:34:31 +0200

    Minor Code Cleanup

--- dev_ws/src/virtual_maize_field/virtual_maize_field/save_img.py
+++ dev_ws/src/virtual_maize_field/virtual_maize_field/save_img.py
@@ -8,43 +8,39 @@ import time
 class ImageSubscriber(Node):
   def __init__(self):
     self.counter=[False, False, False, False]
-    super().__init__('save_img')
+    self.timestamp = int(time.time())
     self.path="/field_robot/src/virtual_maize_field/ai_imgs/"
-    self.subscription0 = self.create_subscription(
+    super().__init__('save_img')
+    self.create_subscription(
       Image, 
       '/robot/camera_back/image_raw', 
       self.listener_callback0, 
       10)
-    self.subscription1 = self.create_subscription(
+    self.create_subscription(
       Image, 
       '/robot/camera_front/image_raw', 
       self.listener_callback1, 
       10)
-    self.subscription2 = self.create_subscription(
+    self.create_subscription(
       Image, 
       '/robot/camera_left/image_raw', 
       self.listener_callback2, 
       10)
-    self.subscription3 = self.create_subscription(
+    self.create_subscription(
       Image, 
       '/robot/camera_right/image_raw', 
       self.listener_callback3, 
       10)
-    self.subscription0
-    self.subscription1
-    self.subscription2
-    self.subscription3
     self.br = CvBridge()
    
   def listener_callback(self, data, id):
     if self.counter[id]==False:
-      self.get_logger().info('Receiving video frame')
       current_frame = self.br.imgmsg_to_cv2(data)
-      cv2.imwrite(self.path+time.ctime(time.time())+str(id)+".png", current_frame)
-      self.get_logger().info('img saved')
+      cv2.imwrite(f"{self.path}/{self.timestamp}-{id}.png", current_frame)
+      self.get_logger().info(f'Image {id} saved')
       self.counter[id]=True
-    if self.counter[0] and self.counter[1] and self.counter[2] and self.counter[3]:
-      self.shutdown()
+      if self.counter[0] and self.counter[1] and self.counter[2] and self.counter[3]:
+        self.shutdown()
 
   def listener_callback0(self, data):
     self.listener_callback(data, 0)
