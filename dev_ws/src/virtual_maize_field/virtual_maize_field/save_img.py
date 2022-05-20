9e17f5e4a297b8af61608861e7e871be32824e8d
Simon Sure <info@simonsure.com>
2022-05-20 13:00:33 +0200

    changing import order
    the CV bridge only works properly when being imported after cv2

--- dev_ws/src/virtual_maize_field/virtual_maize_field/save_img.py
+++ dev_ws/src/virtual_maize_field/virtual_maize_field/save_img.py
@@ -1,8 +1,8 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
 import cv2
+from cv_bridge import CvBridge
 import time
  
 class ImageSubscriber(Node):
