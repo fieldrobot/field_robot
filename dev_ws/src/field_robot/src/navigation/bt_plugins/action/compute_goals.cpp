f060f1e01c13e695f9dbd510189260f7a0922043
Simon Sure <info@simonsure.com>
2022-05-27 12:53:13 +0200

    creating compute goals BT nodes for different navigation parts

---
+++ dev_ws/src/field_robot/src/navigation/bt_plugins/action/compute_goals.cpp
@@ -0,0 +1,52 @@
+#include "nav2_behavior_tree/bt_action_node.hpp"
+
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "field_robot/action/compute_goal.hpp"
+
+class ComputeGoal : public nav2_behavior_tree::BtActionNode<field_robot::action::ComputeGoal>
+{
+    public:
+        ComputeGoal(const std::string & name, const std::string & action_name, const BT::NodeConfiguration & conf)
+            : BtActionNode<field_robot::action::ComputeGoal>(name, action_name, conf)
+        {
+
+        }
+    
+        void on_tick() override
+        {
+
+        }
+
+        static BT::PortsList providedPorts()
+        {
+            return {
+              BT::OutputPort<geometry_msgs::msg::PoseStamped>("goal")
+            };
+        }
+
+    private:
+};
+
+#include "behaviortree_cpp_v3/bt_factory.h"
+BT_REGISTER_NODES(factory)
+{
+  BT::NodeBuilder builder;
+
+  builder = [](const std::string & name, const BT::NodeConfiguration & config)
+  {
+    return std::make_unique<ComputeGoal>(name, "navigation/compute_goal/in_row", config);
+  };
+  factory.registerBuilder<ComputeGoal>("ComputeGoalInRow", builder);
+
+  builder = [](const std::string & name, const BT::NodeConfiguration & config)
+  {
+    return std::make_unique<ComputeGoal>(name, "navigation/compute_goal/leaving_row", config);
+  };
+  factory.registerBuilder<ComputeGoal>("ComputeGoalLeavingRow", builder);
+
+  builder = [](const std::string & name, const BT::NodeConfiguration & config)
+  {
+    return std::make_unique<ComputeGoal>(name, "navigation/compute_goal/out_of_row", config);
+  };
+  factory.registerBuilder<ComputeGoal>("ComputeGoalOutOfRow", builder);
+}
