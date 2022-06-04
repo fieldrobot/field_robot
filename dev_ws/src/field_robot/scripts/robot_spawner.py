f5cae9953b81847e2441268de65595bd5fb300a0
Simon Sure <info@simonsure.com>
2022-06-04 23:53:00 +0200

    updating robot spawn position according to new field

--- dev_ws/src/field_robot/scripts/robot_spawner.py
+++ dev_ws/src/field_robot/scripts/robot_spawner.py
@@ -34,8 +34,8 @@ def main():
     request.robot_namespace = 'robot'
     pose = Pose()
     position = Point()
-    position.x = -2.5713049
-    position.y = -5.0
+    position.x = -4.713934
+    position.y = -6.547766
     position.z = 0.3
     orientation = Quaternion()
     orientation.x = 0.0
