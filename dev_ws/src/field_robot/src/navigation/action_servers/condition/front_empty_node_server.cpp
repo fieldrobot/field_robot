a222f4e018ffafc75d40718565a9f84861c2e4b5
Simon Sure <info@simonsure.com>
2022-06-07 12:24:15 +0200

    creating updated from_empty_server behavior

--- dev_ws/src/field_robot/src/navigation/action_servers/condition/front_empty_node_server.cpp
+++ dev_ws/src/field_robot/src/navigation/action_servers/condition/front_empty_node_server.cpp
@@ -2,6 +2,13 @@
 #include <memory>
 #include <thread>
 
+#include <pcl/common/distances.h>
+#include <pcl/impl/point_types.hpp>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/common/centroid.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/passthrough.h>
+
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 
@@ -19,18 +26,26 @@ class FrontEmptyActionServer : public rclcpp::Node
 
             this->declare_parameter("action_topic", "front_empty");
             this->get_parameter("action_topic", action_topic);
-            this->declare_parameter("front_cloud_topic", "front_cloud");
-            this->get_parameter("front_cloud_topic", front_cloud_topic);
+            this->declare_parameter("left_cloud_topic", "left_cloud");
+            this->get_parameter("left_cloud_topic", left_cloud_topic);
+            this->declare_parameter("right_cloud_topic", "right_cloud");
+            this->get_parameter("right_cloud_topic", right_cloud_topic);
 
             auto qos = rclcpp::QoS(rclcpp::KeepLast(10));
             qos.best_effort();
             qos.durability_volatile();
             qos.lifespan(rclcpp::Duration(1, 0));
 
-            subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-                front_cloud_topic,
+            subscription_left = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+                left_cloud_topic,
                 qos,
-                std::bind(&FrontEmptyActionServer::topic_callback, this, std::placeholders::_1)
+                std::bind(&FrontEmptyActionServer::topic_callback_left, this, std::placeholders::_1)
+            );
+
+            subscription_right = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+                right_cloud_topic,
+                qos,
+                std::bind(&FrontEmptyActionServer::topic_callback_right, this, std::placeholders::_1)
             );
             
             action_server_ = rclcpp_action::create_server<ConditionAction>(
@@ -44,16 +59,24 @@ class FrontEmptyActionServer : public rclcpp::Node
 
     private:
         rclcpp_action::Server<ConditionAction>::SharedPtr action_server_;
-        rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_;
+        rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_left;
+        rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_right;
 
         std::string action_topic;
-        std::string front_cloud_topic;
+        std::string right_cloud_topic;
+        std::string left_cloud_topic;
 
-        sensor_msgs::msg::PointCloud2 cloud_;
+        sensor_msgs::msg::PointCloud2 left_cloud_;
+        sensor_msgs::msg::PointCloud2 right_cloud_;
 
-        void topic_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
+        void topic_callback_left(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
         {
-            cloud_ = *msg;
+            left_cloud_ = *msg;
+        }
+
+        void topic_callback_right(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
+        {
+            right_cloud_ = *msg;
         }
         
         rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const ConditionAction::Goal> goal)
@@ -76,9 +99,43 @@ class FrontEmptyActionServer : public rclcpp::Node
         void execute(const std::shared_ptr<GoalHandleConditionAction> goal_handle)
         {
             auto result = std::make_shared<ConditionAction::Result>();
+
+            pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud_left_ (new pcl::PointCloud<pcl::PointXYZ>);
+            pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud_right_ (new pcl::PointCloud<pcl::PointXYZ>);
+            pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud_left_filtered_a (new pcl::PointCloud<pcl::PointXYZ>);
+            pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud_right_filtered_a (new pcl::PointCloud<pcl::PointXYZ>);
+            pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud_left_filtered_b (new pcl::PointCloud<pcl::PointXYZ>);
+            pcl::PointCloud<pcl::PointXYZ>::Ptr point_cloud_right_filtered_b (new pcl::PointCloud<pcl::PointXYZ>);
+
             while(!goal_handle->is_canceling())
             {
-                if (cloud_.data.size() == 0)
+
+                pcl::fromROSMsg(left_cloud_, *point_cloud_left_);
+                pcl::fromROSMsg(right_cloud_, *point_cloud_right_);
+
+                pcl::PassThrough<pcl::PointXYZ> pass_left_a;
+                pass_left_a.setInputCloud(point_cloud_left_);
+                pass_left_a.setFilterFieldName ("x");
+                pass_left_a.setFilterLimits (-1.0, 0.0);
+                pass_left_a.filter(*point_cloud_left_filtered_a);
+                pcl::PassThrough<pcl::PointXYZ> pass_left_b;
+                pass_left_b.setInputCloud(point_cloud_left_filtered_a);
+                pass_left_b.setFilterFieldName ("y");
+                pass_left_b.setFilterLimits (-0.5, 0.5);
+                pass_left_b.filter(*point_cloud_left_filtered_b);
+
+                pcl::PassThrough<pcl::PointXYZ> pass_right_a;
+                pass_right_a.setInputCloud(point_cloud_right_);
+                pass_right_a.setFilterFieldName ("x");
+                pass_right_a.setFilterLimits (0.0, 1.0);
+                pass_right_a.filter(*point_cloud_right_filtered_a);
+                pcl::PassThrough<pcl::PointXYZ> pass_right_b;
+                pass_right_b.setInputCloud(point_cloud_right_filtered_a);
+                pass_right_b.setFilterFieldName ("y");
+                pass_right_b.setFilterLimits (-0.5, 0.5);
+                pass_right_b.filter(*point_cloud_right_filtered_b);
+
+                if ((point_cloud_right_filtered_b->size() == 0) && (point_cloud_left_filtered_b->size() == 0))
                 {
                     result->success = true;
                     goal_handle->succeed(result);
