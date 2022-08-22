074df145524ccd987867bf7284bce44c0ef38476
Simon Sure <info@simonsure.com>
2022-08-22 15:17:40 +0200

    changing includes and buildes for ROS2 release humble

--- dev_ws/src/field_robot/src/point_cloud_generator.cpp
+++ dev_ws/src/field_robot/src/point_cloud_generator.cpp
@@ -11,7 +11,7 @@
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
 
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 #include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/msg/camera_info.hpp>
